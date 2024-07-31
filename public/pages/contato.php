15e56373d5f7be608f75074ebfb65583c926aff1
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-31 09:35:37 -0300

    Criando o formulário de contato

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -1,1 +1,47 @@
 <h2>Contato</h2>
+
+<form class="d-flex" action="../pages/forms/contato.php" method="POST" role="form">
+    <div class="col">
+        <legend>Formulário de Contato</legend>
+        <div class="form-group mb-3">
+            <label for="" class="form-label">Nome:</label>
+            <input
+                type="text"
+                name="name"
+                id=""
+                class="form-control"
+                placeholder=""
+            >
+        </div>
+        <div class="form-group mb-3">
+            <label for="" class="form-label">Email:</label>
+            <input
+                type="email"
+                name="email"
+                id=""
+                class="form-control"
+                placeholder=""
+            >
+        </div>
+        <div class="form-group mb-3">
+            <label for="" class="form-label">Assunto:</label>
+            <input
+                type="text"
+                name="subject"
+                id=""
+                class="form-control"
+                placeholder=""
+            >
+        </div>
+        <div class="form-group mb-3">
+            <label for="" class="form-label">Mensagem:</label>
+            <textarea class="form-control" name="message" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button
+            type="submit"
+            class="btn btn-primary"
+        >
+            Submit
+        </button>
+    </div>
+</form>
