fdcdf780e118697999ed533a36be20358f18e89c
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-08-01 10:11:11 -0300

    Criando a função de validação do formulário

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -1,5 +1,25 @@
 <?php
 
 function validate(array $fields) {
-    for
+    
+    $validate = [];
+    
+    foreach ($fields as $field => $type) {
+        switch ($type) {
+            case 's':
+                $validate[$field] = filter_var($_POST[$field]);
+                break;
+            
+            case 'i':
+                $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_NUMBER_INT);
+                break;
+            
+            case 'e':
+                $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_EMAIL);
+                break;
+            
+        }
+    }
+    
+    return (object) $validate;
 }
