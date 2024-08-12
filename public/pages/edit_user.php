0f4f5555da7905133a5328e4ee2fad7a6ecd1790
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-08-12 09:46:49 -0300

    Criando o formulário de edição

---
+++ public/pages/edit_user.php
@@ -0,0 +1,28 @@
+<h1>Atualizar Cadastro de Usuários</h1>
+<br><?= get('message'); ?><br><br>
+
+<?php
+    $user = find('users', 'id', $_GET['id']);
+?>
+
+<form action="../pages/forms/update_user.php" method="POST" role="form" class="d-flex form-group">
+    <div class="">
+        <div class="mb-3">
+            <label for="" class="form-label">Nome</label>
+            <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?= $user->name; ?>" />
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Sobrenome</label>
+            <input type="text" name="lastname" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?= $user->lastname; ?>" />
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">E-mail</label>
+            <input type="text" name="email" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?= $user->email; ?>" />
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Senha</label>
+            <input type="text" name="passwd" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?= $user->passwd; ?>" />
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </div>
+</form>
