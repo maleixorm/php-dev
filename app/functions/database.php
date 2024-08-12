cb9be7a0d680203149014f9afa5fc560ac61e10a
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-08-12 16:53:02 -0300

    Criando a função que atualiza os dados

--- app/functions/database.php
+++ app/functions/database.php
@@ -31,8 +31,22 @@ function all($table) {
     return $list->fetchAll();
 }
 
-function update() {
-
+function update($table, $fields, $where) {
+    if (!is_array($fields)) {
+        $fields = (array) $fields;
+    }
+    $pdo = connect();
+    $data = array_map(function ($field) {
+        return "{$field} = :{$field}";
+    }, array_keys($fields));
+    $sql = "UPDATE {$table} SET ";
+    $sql .= implode(',', $data);
+    $sql .= " WHERE {$where[0]} = :{$where[0]}";
+    //dd($sql);
+    $data = array_merge($fields, [$where[0] => $where[1]]);
+    $update = $pdo->prepare($sql);
+    $update->execute($data);
+    return $update->rowCount();
 }
 
 function find($table, $field, $value) {
